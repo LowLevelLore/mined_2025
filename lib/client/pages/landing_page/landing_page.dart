0923db85a1e40232d9c8b65521952df2d53a83c1
harsh <harshmori0521@gmail.com>
2025-01-31 00:56:00 +0530

    landing page created

--- lib/client/pages/landing_page/landing_page.dart
+++ lib/client/pages/landing_page/landing_page.dart
@@ -1,23 +1,169 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mined_2025/client/providers/bucket_provider.dart';
+import 'package:mined_2025/client/utils/custom_containers/custom_contaner.dart';
 import 'package:mined_2025/client/utils/theme/theme.dart';
+import 'package:provider/provider.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   static const route = "/";
   static const fullRoute = "/";
   const LandingPage({super.key});
 
   @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  bool _isRegisterHover = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      color: AppColors.theme['backgroundColor'],
-      child: const Center(
-        child: Text(
-          "Landing Page",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-      ),
-    );
+    return Consumer<BucketsProvider>(builder: (context, bucketProvider, child) {
+      return Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: AppColors.theme['backgroundColor'],
+          ),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                        child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 50.0, top: 80),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Make your",
+                              style: GoogleFonts.poppins(
+                                fontSize: 40,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Research 2X",
+                              style: GoogleFonts.poppins(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              "Paper2x provides you tools ",
+                              style: GoogleFonts.poppins(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text(
+                              "that helps you to make research easy",
+                              style: GoogleFonts.poppins(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "you can",
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 3.0),
+                                  child: MouseRegion(
+                                    cursor: SystemMouseCursors.click,
+                                    onEnter: (_) =>
+                                        setState(() => _isRegisterHover = true),
+                                    onExit: (_) => setState(
+                                        () => _isRegisterHover = false),
+                                    child: Column(
+                                      children: [
+                                        GestureDetector(
+                                          onTap: () {
+                                            context.go('/register');
+                                            bucketProvider.isLoginEnabled =
+                                                false;
+                                          },
+                                          child: Text(
+                                            "Register here!",
+                                            style: GoogleFonts.poppins(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold,
+                                              color: !_isRegisterHover
+                                                  ? AppColors
+                                                      .theme['deepPurpleColor']
+                                                  : Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 2.0),
+                                          child: AnimatedContainer(
+                                            duration: const Duration(
+                                                milliseconds: 300),
+                                            height: 2,
+                                            width:
+                                                (_isRegisterHover) ? 80.0 : 0.0,
+                                            decoration: BoxDecoration(
+                                              color: AppColors
+                                                  .theme['deepPurpleColor'],
+                                              borderRadius:
+                                                  BorderRadius.circular(2),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    )),
+                    Image.asset("assets/illustrations/landing_page.png"),
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Features",
+                      style: GoogleFonts.poppins(
+                          fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        HoverCard("Demo", Colors.green, Icons.chat,counter: 1,),
+                        HoverCard("Demo", Colors.deepPurple, Icons.picture_as_pdf_outlined,counter: 2,),
+                        HoverCard("Demo", Colors.pinkAccent, Icons.video_camera_back_outlined,counter: 3,),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ));
+    });
   }
 }
---
+++ lib/client/utils/custom_containers/custom_contaner.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mined_2025/client/utils/theme/theme.dart';
+
+class HoverCard extends StatefulWidget {
+  final String title;
+  final Color color;
+  final IconData icon;
+  final int counter;
+
+  HoverCard(this.title, this.color, this.icon, {this.counter = 0});
+
+  @override
+  _HoverCardState createState() => _HoverCardState();
+}
+
+class _HoverCardState extends State<HoverCard> {
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        onEnter: (_) => setState(() => isHovered = true),
+        onExit: (_) => setState(() => isHovered = false),
+        child: Transform.scale(
+          scale: isHovered ? 1.1 : 1.0,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                padding: EdgeInsets.all(20),
+                width: 300,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: isHovered ? widget.color.withOpacity(0.3) : AppColors.theme['backgroundColor'],
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: widget.color, width: 2),
+                  boxShadow: isHovered
+                      ? [
+                    BoxShadow(
+                      color: widget.color.withOpacity(0.5),
+                      blurRadius: 12,
+                      spreadRadius: 3,
+                    )
+                  ]
+                      : [],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+
+                        Transform.scale(
+                          scale: isHovered ? 1.2 : 1.0,
+                          child: Icon(
+                            widget.icon,
+                            size: 20,
+                            color: isHovered ? widget.color : Colors.black,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        AnimatedDefaultTextStyle(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeInOut,
+                          style: TextStyle(
+                            color: isHovered ? widget.color : Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          child: Text(widget.title),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Flexible(
+                      child: Text(
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: isHovered ? widget.color : Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: -10,
+                right: -10,
+                child: CircleAvatar(
+                  radius: 20,
+                  backgroundColor: widget.color,
+                  child: Text(
+                    widget.counter.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
